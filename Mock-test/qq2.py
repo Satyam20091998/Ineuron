c4fd87a40f3c3d846d444a07ad7ecae5e4fca4b4
Satyam Markam <92753984+Satyam20091998@users.noreply.github.com>
2023-06-25 18:06:04 +0530

    Create qq2.py

---
+++ Mock-test/qq2.py
@@ -0,0 +1,26 @@
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    def dequeue(self):
+        if self.is_empty():
+            return "Queue is empty"
+        return self.queue.pop(0)
+
+    def is_empty(self):
+        return len(self.queue) == 0
+
+queue = Queue()
+queue.enqueue(10)
+queue.enqueue(20)
+queue.enqueue(30)
+
+print(queue.dequeue())  # Output: 10
+print(queue.is_empty())  # Output: False
+print(queue.dequeue())  # Output: 20
+print(queue.dequeue())  # Output: 30
+print(queue.is_empty())  # Output: True
+print(queue.dequeue())  # Output: Queue is empty
